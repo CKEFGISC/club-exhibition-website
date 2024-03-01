1f4c9687efaa68720f631034b164356d5e2c610a
Lily Kensa <berhew.cheng@gmail.com>
2024-03-01 23:04:08 +0800

    Add a temp page

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,6 @@ import { createHashRouter } from "react-router-dom";
 
 export default function createIndexRouter() {
   return createHashRouter([
-    { path: "/", element: <>123</> },
+    { path: "/", element: <div className="text-center p-5"><h1>This Page is Empty</h1></div> },
   ]);
 }
